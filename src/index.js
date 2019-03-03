06470ad70026a1442e8baff6bce209270e1d6ead
Арина <arina.senchuk@gmail.com>
2019-03-03 17:13:13 +0300

    did the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    else {
+        var stack =[];
+
+        const stroke = str.split('');
+        const len = stroke.length;
+
+        for(var i = 0; i < len; i++) {
+            for (var j = 0; j < bracketsConfig.length; j++) {
+                if (stroke[i] === bracketsConfig[j][0] && stroke[i] === bracketsConfig[j][1]) {
+                    if(stack.length === 0) {
+                        stack.push(stroke[i]);
+                        break;
+                    } else {
+                        el = stack.pop();
+                        if (el === stroke[i]) {
+                            break;
+                        } else {
+                            stack.push(el);
+                            stack.push(stroke[i]);
+                            break;
+                        }
+                    }
+                }
+                if (stroke[i] === bracketsConfig[j][0])  {
+                    stack.push(stroke[i]);
+                    break;
+                }
+                else if(stroke[i] === bracketsConfig[j][1]){
+                    if (stack.length === 0) {
+                        return false;
+                    }
+                    else {
+                        el = stack.pop();
+                        if (el !== bracketsConfig[j][0]) {
+                            return false;
+
+                        }
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        return stack.length === 0;
+    }
+};
